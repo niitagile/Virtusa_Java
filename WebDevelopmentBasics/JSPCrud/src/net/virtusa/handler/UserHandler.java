6057046bb8a7315b4debdd5ea09787f219d937bd
niitagile <43024343+niitagile@users.noreply.github.com>
2022-07-18 11:06:25 +0530

    Add files via upload

--- WebDevelopmentBasics/JSPCrud/src/net/virtusa/handler/UserHandler.java
+++ WebDevelopmentBasics/JSPCrud/src/net/virtusa/handler/UserHandler.java
@@ -24,8 +24,43 @@ public class UserHandler extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
        
+    	String action = request.getParameter("action");
+    	if(action.equalsIgnoreCase("insert")) {
+    		String uId = request.getParameter("userid");
+    		int id = Integer.parseInt(uId);
+    		String firstname=request.getParameter("firstName");
+    		String lastname=request.getParameter("lastName");
+    		UserBean user = new UserBean();
+    		user.setId(id);
+    		user.setfName(firstname);
+    		user.setlName(lastname);
+    		dao.addUser(user);
+    		request.setAttribute("users", dao.getAllUsers()); 
+    		RequestDispatcher rd = request.getRequestDispatcher("/listUser.jsp");
+            rd.forward(request, response);
+    	}
     	
-    	
+    	if (action.equalsIgnoreCase("delete")){
+            String userId = request.getParameter("userId");
+            int uid = Integer.parseInt(userId);
+            dao.removeUser(uid);
+           
+            request.setAttribute("users", dao.getAllUsers());
+            RequestDispatcher rd = request.getRequestDispatcher("/listUser.jsp");
+            rd.forward(request, response);
+        }
+    	if (action.equalsIgnoreCase("edit")){
+        	String userId = request.getParameter("userId");
+            int uid = Integer.parseInt(userId);            
+            UserBean user = new UserBean();
+        	user.setId(uid);
+            user.setfName(request.getParameter("firstName"));
+            user.setlName(request.getParameter("lastName"));
+            dao.editUser(user);
+            request.setAttribute("users", dao.getAllUsers());
+            RequestDispatcher rd = request.getRequestDispatcher("/listUser.jsp");
+            rd.forward(request, response);
+         }
     	
     }
 
